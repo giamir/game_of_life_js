b9758f6bf2c29735eff59b2d0b7e7b2846b4f9e1
Giamir Buoncristiani <giamir.buoncristiani@gmail.com>
2016-03-14 09:08:35 +0000

    implement makeRandom method for LifeBoard class

--- public/js/lifeBoard.js
+++ public/js/lifeBoard.js
@@ -11,6 +11,14 @@ LifeBoard.prototype.set = function() {
   return board;
 }
 
+LifeBoard.prototype.makeRandom = function() {
+  for(var i=0; i<this.board.length; i++) {
+    for(var j=0; j<this.board[i].length; j++) {
+      this.board[i][j] = Math.random() < 0.5;
+    }
+  }
+}
+
 LifeBoard.prototype.defaults = {
   xsize: 10,
   ysize: 5
--- test/lifeBoard.spec.js
+++ test/lifeBoard.spec.js
@@ -19,4 +19,14 @@ describe('LifeBoard', function() {
       expect(board[0]).not.toContain(true);
     });
   });
+
+  describe('#makeRandom', function() {
+    it('populate the board (matrix) randomly', function() {
+      lifeBoard.set();
+      spyOn(Math, 'random').and.returnValue(0.4);
+      lifeBoard.makeRandom();
+      expect(lifeBoard.board[0]).toContain(true);
+      expect(lifeBoard.board[0]).not.toContain(false);
+    });
+  });
 });
